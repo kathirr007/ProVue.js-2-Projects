95591b43baaf80cdcd057bca8eb46f5bd3f0623c
kkumarasamy <kkumarasamy@edit-place.com>
2018-12-07 17:04:44 +0530

    Added filtering of completed tasks from the list of tasks

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -8,11 +8,19 @@
         <div class="col font-weight-bold">Task</div>
         <div class="col-2 font-weight-bold">Done</div>
       </div>
-      <div class="row" v-for="task in tasks" v-bind:key="task.action">
+      <div class="row" v-for="task in filteredTasks" v-bind:key="task.action">
         <div class="col">{{task.action}}</div>
         <div class="col-2">
           <input type="checkbox" class="form-check-input" v-model="task.done" />
           {{task.done}}
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input id="hideCompleted" type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label for="hideCompleted" class="form-check-label font-weight-bold">
+            Hide completed tasks
+          </label>
         </div>
       </div>
     </div>
@@ -33,6 +41,13 @@ export default {
         {action: "Driving class training", done: false},
         {action: "Cook Non-veg", done: false}
       ],
+      hideCompleted: true,
+    }
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted ?
+        this.tasks.filter(task => !task.done) : this.tasks;
     }
   }
 }
