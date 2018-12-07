908b465af31bfc232c76338dc237587c75c919be
kkumarasamy <kkumarasamy@edit-place.com>
2018-12-07 16:49:29 +0530

    Show list of tasks from data using v-for directive

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,15 +1,37 @@
 <template>
   <div id="app">
     <h4 class="bg-primary p-2 text-white text-center">
-      To do list
+      {{name}}'s' To do list
     </h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="task in tasks" v-bind:key="task.action">
+        <div class="col">{{task.action}}</div>
+        <div class="col-2">{{task.done}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
+  data() {
+    return {
+      name: "Kathirr007",
+      tasks: [
+        {action: "Buy Vegetables", done: true},
+        {action: "Buy Medicines", done: false},
+        {action: "Buy Books", done: true},
+        {action: "Driving class training", done: false},
+        {action: "Cook Non-veg", done: false}
+      ],
+    }
+  }
 }
 </script>
 
